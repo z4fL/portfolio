69b9744e9e792527d10e025d6281ccd8a17b76ae
z4fL <zaaaafl654@gmail.com>
2026-09-18 16:24:17 +0700

    chore: update dependencies and lint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,10 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 
 export default [
-  { ignores: ["dist"] },
+  { ignores: [
+    "dist/**",
+    "public/old/**"
+  ] },
   {
     files: ["**/*.{js,jsx}"],
     languageOptions: {
@@ -17,7 +20,7 @@ export default [
         sourceType: "module",
       },
     },
-    settings: { react: { version: "18.3" } },
+    settings: { react: { version: "19.3" } },
     plugins: {
       react,
       "react-hooks": reactHooks,
